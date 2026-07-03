131e7dc0fa6b07d76082672abb480c20eb91d7ee
Davi Souza <davimj99@gmail.com>
2026-07-02 21:01:24 -0300

    fix: correção de deploy superuser

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
-
 set -o errexit
 
 python manage.py migrate
 
-python manage.py createsuperuser --noinput || true
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+EOF
