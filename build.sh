7a584635c54ddaa4cb422165aa64424a5f9c149c
davisouza99 <davisouza.pro@hotmail.com>
2026-07-02 21:36:24 -0300

    fix: correção e implemantando atualização

--- build.sh
+++ build.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python manage.py migrate
-
-python manage.py shell << EOF
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "davisouza.pro@hotmail.com")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "123456")
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(
-        username=username,
-        email=email,
-        password=password
-    )
-    print("Superuser criado com sucesso")
+python manage.py migrate --noinput
 else:
     print("Superuser já existe")
 EOF
