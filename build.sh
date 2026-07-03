c3b9fd346e8ae28778dbc9022e6b7cf7fc34f649
Davi Souza <davimj99@gmail.com>
2026-07-02 21:09:33 -0300

    fix: bug não renderizado, forçando cache

--- build.sh
+++ build.sh
@@ -9,15 +9,17 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@email.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "123456")
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+    print("Superuser criado com sucesso")
+else:
+    print("Superuser já existe")
 EOF
