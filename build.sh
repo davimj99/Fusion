3814b686bea8144d13d164c885cf78cbce6ed755
Loja Online <davisouza.pro@hotmail.com>
2026-07-02 23:30:48 -0300

    perf: melhorando performance

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install -r requirements.txt
+
 python manage.py migrate --noinput
+
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+email = "admin@email.com"
+password = "123456"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser criado")
 else:
     print("Superuser já existe")
 EOF
+
+python manage.py collectstatic --noinput
